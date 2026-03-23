f8f784f09e9f9ff3cb04b28350eadf0ceca9652f
Inass Ouadrhiri <inassouadrhiri17@gmail.com>
2026-03-23 14:32:38 +0100

    algo genetique : ajout de la fonction qui crée les directions de vecteurs et fct qui modifie un attribut

--- source/latent_space.py
+++ source/latent_space.py
@@ -2,6 +2,9 @@ import numpy as np #bibliotheque pour manipuler tableaux numeriques
 from PIL import Image #bibliotheque pour manipulation d'images
 import numpy as np #bibliotheque pour manipuler tableaux numeriques
 
+#-----------------------------------------------------------------------------
+# Reconstruction d'images  
+#-----------------------------------------------------------------------------
 def reconstruct_image(image):
     """
     Cette fonction prend une image en entrée
@@ -17,7 +20,9 @@ def reconstruct_image(image):
 
     return Image.fromarray(reconstructed)
 
-
+#-----------------------------------------------------------------------------
+# Interpolation d'images
+#-----------------------------------------------------------------------------
 def interpolate_images(img1, img2):
     """
     Placeholder interpolation espace latent
@@ -31,3 +36,98 @@ def interpolate_images(img1, img2):
     blend = (0.5 * img1 + 0.5 * img2).astype(np.uint8)
 
     return Image.fromarray(blend)
+
+#-----------------------------------------------------------------------------
+# Mélange d'images 
+#-----------------------------------------------------------------------------
+
+
+
+#-----------------------------------------------------------------------------
+# Ajoute ou suppression d'une caractéristique 
+#-----------------------------------------------------------------------------
+features = ['5_o_Clock_Shadow', 'Arched_Eyebrows', 'Attractive', 'Bags_Under_Eyes', 'Bald', 'Bangs', 'Big_Lips', 'Big_Nose', 'Black_Hair', 'Blond_Hair',
+             'Blurry', 'Brown_Hair', 'Bushy_Eyebrows', 'Chubby', 'Double_Chin', 'Eyeglasses', 'Goatee', 'Gray_Hair', 'Heavy_Makeup', 'High_Cheekbones',
+            'Male','Mouth_Slightly_Open','Mustache', 'Narrow_Eyes', 'No_Beard', 'Oval_Face', 'Pale_Skin', 'Pointy_Nose', 'Receding_Hairline',
+            'Rosy_Cheeks','Sideburns','Smiling', 'Straight_Hair', 'Wavy_Hair', 'Wearing_Earrings', 'Wearing_Hat', 'Wearing_Lipstick', 'Wearing_Necklace', 'Wearing_Necktie','Young' ]
+
+def load_attributs(txt_file = 'list_attr_celeba.txt'): #ajouter le dataset et ce fichier
+    '''    
+    Charge les attributs en binaire (-1 ou 1) pour chaque image
+    Retour:
+        Dict {image_id: [features]}
+    '''    
+    dico_attributs = {}
+    images_id = []
+    with open (txt_file, 'r') as file : 
+        for line in file : 
+            parties = line.strip().split()
+            image_id = parties[0] #id de l'image 
+            images_id.append(image_id)
+            
+            features = []
+            for x in parties[1:]:
+                features.append(x) #creation des listes des attributs binaires pour l'image
+            dico_attributs[image_id] = features #ajout dans le dico de l'image et de ses features
+    return images_id, dico_attributs
+
+
+
+def compute_directions(features, images_id, dico_attributs, vecteurs_latents): 
+    '''
+    Calcule les directions pour chaque caractéristique (chaque feature)
+    
+    Parametres : 
+        features : liste de str de tous les attributs
+        images_id : liste des id de toutes les images
+        vecteurs_latents : array de dimensions (nbr_images,nbr de latent_dim) qui contient les vecteurs latents de chaque image après AE
+        dico_attributs : dict{image_id : [features]} qui contient tous les attributs de chaque image (en binaires : -1 ou 1)
+
+    Retour : 
+        dico_direction : dict{feature : vecteur_direction} qui contient pour chaque feature (Blond_hair,...) le vecteur de direction correspondant 
+    
+    ''' 
+    dico_direction = {}
+    
+    for i, feature in enumerate(features) : #pour chaque attribut
+        with_feature = []
+        without_feature = []
+        
+        for j, id in enumerate(images_id): #on regarde si l'attribut est présent ou pas dans chaque image 
+            if dico_attributs['id'][i] == 1 : #l'attribut est présent dans l'image 
+                with_feature.append(vecteurs_latents[j]) #on ajoute le vecteur latent correspondant à cette image dans la liste des 'with_features'
+            elif dico_attributs['id'][i] == -1 : #l'attribut n'est pas présent dans l'image 
+                without_feature.append(vecteurs_latents[j]) #on ajoute le vecteur latent correspondant à cette image dans la liste des 'without_features'
+        
+        # Moyennes des vecteurs latents dans with_feature et without_features 
+        moy_with = np.mean(with_feature, axis = 0)
+        moy_without = np.mean(without_feature, axis = 0)
+
+        # Computing la direction en faisant la soustraction de with et de without : 
+        direction = moy_with - moy_without
+        dico_direction['feature'] = direction
+    
+    return dico_direction
+
+
+
+def modif_attribut(latent_original , dico_direction, attribut, modification, alpha = 1.0): 
+    '''
+    Fonction qui modifie une image en ajoutant ou en supprimant un attribut donné 
+    modification : ajout ou suppression (de l'attribut)
+
+    '''
+    if modification == 'ajout' : 
+        nouv_latent = latent_original + dico_direction['attribut']
+    elif modification == 'suppression' :
+        nouv_latent = latent_original - dico_direction['attribut']
+
+    return nouv_latent 
+
+#autre version : on propose à l'utilisateur de choisir plusieurs attributs à modifier parmi une liste (voir si on donne les noms tels quels dans la liste features ou alors on donne d'autres noms avec explication et on fait le lien nous même)
+#pour faire plusieurs modifications de plusieurs attributs -> boucles 
+    
+
+
+
+
